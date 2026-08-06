877f645e88a9909300faf3701951000b606f14f9
fuad agayev <fuad0000010@gmail.com>
2026-08-06 15:14:36 +0400

    w-64 second commit same

--- frontend/app/components/AudioPlayer.vue
+++ frontend/app/components/AudioPlayer.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <audio v-if="props.src" controls class="sm:w-64 md:w-80 lg:w-96 h-12 mt-2">
+  <audio v-if="props.src" controls class="w-64 sm:w-64 md:w-80 lg:w-96 h-12 mt-2">
   <source :src="props.src" type="audio/mpeg" />
   <source :src="props.src" type="audio/m4a" />
   <source :src="props.src" type="audio/webm" />
