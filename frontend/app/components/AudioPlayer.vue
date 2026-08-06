db239aeb837e6774c6e5f17cf45ba3bfdb57ae0f
Fuad Agayev <123968952+fuad-agayev@users.noreply.github.com>
2026-08-06 13:08:17 +0400

    Update AudioPlayer.vue

--- frontend/app/components/AudioPlayer.vue
+++ frontend/app/components/AudioPlayer.vue
@@ -1,7 +1,7 @@
 
 <template>
   <audio v-if="props.src" controls 
-  class="w-full sm:w-64 md:w-80 lg:w-96 h-10 min-h-10 mt-2 shrink-0">
+  class="w-full sm:w-64 md:w-80 lg:w-96 h-12 mt-2">
   <source :src="props.src" type="audio/mpeg" />
   <source :src="props.src" type="audio/m4a" />
   <source :src="props.src" type="audio/webm" />
